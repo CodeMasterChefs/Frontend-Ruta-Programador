ec397fe05613b2496e86f8a627bfa18839098302
Mariana <marivalleandrea@gmail.com>
2023-09-30 14:40:45 -0400

    Eliminacion codigo irrelevante

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,12 @@
-import "./App.css";
 import { Aniadir } from "./Components/AniadirElemento/Aniadir";
 import { Playlist } from "./Components/NewPlaylist/Playlist";
 import { Eliminar } from "./Components/EliminarElemento/Eliminar";
 
-import { useState } from "react";
-
 //import "./App.css";
-import Button from "./Components/Button/Button";
 import { Routes, Route } from "react-router-dom";
 import HomePage from "./Pages/HomePage/HomePage";
-import CrearPlaylist from "./Components/CrearPlaylist/CrearPlaylist";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
       <Aniadir />
@@ -23,16 +16,6 @@ function App() {
       <Routes>
         <Route path="/" element={<HomePage></HomePage>}></Route>
       </Routes>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-        <Button />
-      </div>
-      <CrearPlaylist></CrearPlaylist>
     </>
   );
 }
