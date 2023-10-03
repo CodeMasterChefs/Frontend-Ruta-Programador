869f4cdba2f56c714e581fa8a2c002cb3af1cf86
Winsor <202108302@est.umss.edu>
2023-10-02 22:03:08 -0400

    add: endpoint GET playlists conectado

--- src/Pages/MisPlaylistsPage/MisPlaylistsPage.jsx
+++ src/Pages/MisPlaylistsPage/MisPlaylistsPage.jsx
@@ -1,6 +1,23 @@
 import Playlist from "../../Components/NewPlaylist/Playlist";
 import { Card } from "../../Components/CardPlanet/Card";
+import axios from "axios";
+import { useEffect, useState } from "react";
+
 const MisPlaylistsPage = () => {
+  const [playlist, setPlaylists] = useState([]);
+  
+
+  const url = "https://backend-rutadelprogramador-production.up.railway.app/api/playlist/2";
+
+  useEffect(() => {
+    axios.get(url).then(
+      (response) => {
+        setPlaylists(response.data);
+        console.log(response.data);
+      }
+    )
+  }, [])
+
   return (
     <main className="col-sm-11">
       <div className="row">
@@ -12,24 +29,33 @@ const MisPlaylistsPage = () => {
       <div className="d-flex p-2"></div>
       <div className="col-12">
         <div className="row row-cols-1 row-cols-md-5 g-4">
-        <Card
+          {
+            playlist.map((playlist) => {
+              return <Card
+                key={playlist.idPlaylist}
+                Titulo={playlist.tituloPlaylist}
+                Descripcion={playlist.descripcionPlaylist}
+              ></Card>
+            })
+          }
+          <Card
             Descripcion="El planeta con mas anillos del sistema solar, el 2do planeta mas grande de nuestro astro sol"
             Titulo="Saturno"
           />
-        <Card Descripcion="x" Titulo="Jupiter" />
-        <Card
+          <Card Descripcion="x" Titulo="Jupiter" />
+          <Card
             Descripcion="El planeta con mas anillos del sistema solar, el 2do planeta mas grande de nuestro astro sol"
             Titulo="Tierra"
-          /> 
-        <Card
+          />
+          <Card
             Descripcion="El planeta con mas anillos del sistema solar, el 2do planeta mas grande de nuestro astro sol"
             Titulo="Mercurio"
           />
-        <Card
+          <Card
             Descripcion="El planeta con mas anillos del sistema solar, el 2do planeta mas grande de nuestro astro sol"
             Titulo="Venus"
           />
-        <Card
+          <Card
             Descripcion="El planeta con mas anillos del sistema solar, el 2do planeta mas grande de nuestro astro sol"
             Titulo="Marte"
           />
