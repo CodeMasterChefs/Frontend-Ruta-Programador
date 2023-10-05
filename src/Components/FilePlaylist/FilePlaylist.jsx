cfe80705e1e8618fc1cd8d2e9988a85aaa1969b8
Emily Callejas <emilycallejastorrico@gmail.com>
2023-10-04 21:16:34 -0400

    Estilos de FilePlaylist y TitDescripcion

--- src/Components/FilePlaylist/FilePlaylist.jsx
+++ src/Components/FilePlaylist/FilePlaylist.jsx
@@ -1,5 +1,5 @@
 import PropTypes from "prop-types";
-
+import "./FilePlaylist.css"
 const Fileplaylist = ({ titulo, fecha, duracion, id }) => {
   return (
     <>
@@ -9,7 +9,7 @@ const Fileplaylist = ({ titulo, fecha, duracion, id }) => {
         <div>{fecha}</div>
         <div>{duracion}</div>
         <div className="dropdown">
-          <button data-bs-toggle="dropdown">
+          <button data-bs-toggle="dropdown" className="dropdown-vertical-button">
             <svg
               width="24"
               height="24"
@@ -20,7 +20,7 @@ const Fileplaylist = ({ titulo, fecha, duracion, id }) => {
               <g id="bx-dots-horizontal-rounded">
                 <path
                   id="Vector"
-                  d="M12 10C10.9 10 10 10.9 10 12C10 13.1 10.9 14 12 14C13.1 14 14 13.1 14 12C14 10.9 13.1 10 12 10ZM18 10C16.9 10 16 10.9 16 12C16 13.1 16.9 14 18 14C19.1 14 20 13.1 20 12C20 10.9 19.1 10 18 10ZM6 10C4.9 10 4 10.9 4 12C4 13.1 4.9 14 6 14C7.1 14 8 13.1 8 12C8 10.9 7.1 10 6 10Z"
+                  d="M12 10C10.9 10 10 10.9 10 12C10 13.1 10.9 14 12 14C13.1 14 14 13.1 14 12C14 10.9 13.1 10 12 10ZM12 4C10.9 4 10 4.9 10 6C10 7.1 10.9 8 12 8C13.1 8 14 7.1 14 6C14 4.9 13.1 4 12 4ZM12 16C10.9 16 10 16.9 10 18C10 19.1 10.9 20 12 20C13.1 20 14 19.1 14 18C14 16.9 13.1 16 12 16Z"
                   fill="#F2F2F2"
                 />
               </g>
