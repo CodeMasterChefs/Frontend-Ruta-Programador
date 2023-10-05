b6ba103c2d5594f162264e8ad4ccc986b9f94ab8
Giulianno Morales <202107698@est.umss.edu>
2023-10-05 17:12:40 -0400

    cabecera de la tabla

--- src/Components/icons/ClockIcon.jsx
+++ src/Components/icons/ClockIcon.jsx
@@ -5,15 +5,15 @@ const SvgComponent = (props) => (
     width={24}
     height={24}
     fill="none"
-    stroke="black"
+    stroke="white"
     strokeLinecap="round"
     strokeLinejoin="round"
     strokeWidth={2}
-    className="icon icon-tabler icon-tabler-plus"
+    className="icon icon-tabler icon-tabler-clock-hour-3"
     {...props}
   >
     <path stroke="none" d="M0 0h24v24H0z" />
-    <path d="M12 5v14M5 12h14" />
+    <path d="M3 12a9 9 0 1 0 18 0 9 9 0 1 0-18 0M12 12h3.5M12 7v5" />
   </svg>
 )
-export { SvgComponent as MoreIcon }
+export { SvgComponent as ClockIcon }
