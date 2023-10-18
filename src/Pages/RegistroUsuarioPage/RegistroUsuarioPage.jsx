6296407e11b97c315d19ecdd985a9edcda011e49
Giulianno Morales <202107698@est.umss.edu>
2023-10-18 15:24:15 -0400

    Add registrarusuarioPage

---
+++ src/Pages/RegistroUsuarioPage/RegistroUsuarioPage.jsx
@@ -0,0 +1,28 @@
+
+import "./RegistroUsuarioPage.css"
+const RegistroUsuarioPage = () => {
+  return (
+    <div>
+    <div className="col-sm-6 px-0">
+     
+    </div>
+    <div className="col-sm-6">
+        
+        
+        <div className="required field cajita">
+        <label>Nombre de Usuario</label>
+        <br></br>
+        <input type="text" placeholder="Full Name"/>
+        <div data-lastpass-icon-root="true"></div></div>
+      
+    
+    </div>
+    </div>
+      
+      
+      
+
+  )
+}
+
+export default RegistroUsuarioPage
