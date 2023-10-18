672197f498e287aeed294e50ddc5617c1b87c7f7
Giulianno Morales <202107698@est.umss.edu>
2023-10-18 15:34:55 -0400

    Change registrarusuarioPage

--- src/Pages/RegistroUsuarioPage/RegistroUsuarioPage.jsx
+++ src/Pages/RegistroUsuarioPage/RegistroUsuarioPage.jsx
@@ -4,18 +4,36 @@ const RegistroUsuarioPage = () => {
   return (
     <div>
     <div className="col-sm-6 px-0">
-     
+
     </div>
-    <div className="col-sm-6">
-        
-        
+    <div className="col-sm-6">  
         <div className="required field cajita">
         <label>Nombre de Usuario</label>
         <br></br>
-        <input type="text" placeholder="Full Name"/>
+        <input type="text" placeholder="Ingresar nombre"/>
         <div data-lastpass-icon-root="true"></div></div>
-      
-    
+        <br></br>
+        <br></br>
+        <div className="required field cajita">
+        <label>Correo electronico</label>
+        <br></br>
+        <input type="text" placeholder="Ingresar correo"/>
+        <div data-lastpass-icon-root="true"></div></div>
+        <br></br>
+        <br></br>
+        <div className="required field cajita">
+        <label>Contraseña</label>
+        <br></br>
+        <input type="password" placeholder="Ingresar contraseña"/>
+        <div data-lastpass-icon-root="true"></div></div>
+        <br></br>
+        <br></br>
+        <div className="required field cajita">
+        <label>Confirmar contraseña</label>
+        <br></br>
+        <input type="password" placeholder="Ingresar contraseña"/>
+        <div data-lastpass-icon-root="true"></div></div>
+        <br></br>
     </div>
     </div>
       
