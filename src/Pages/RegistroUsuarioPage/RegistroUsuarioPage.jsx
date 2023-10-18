d4abb145e760240438f822b42ed8e1474d307e14
Giulianno Morales <202107698@est.umss.edu>
2023-10-18 16:07:51 -0400

    Change registrarusuarioPage,add imagen

--- src/Pages/RegistroUsuarioPage/RegistroUsuarioPage.jsx
+++ src/Pages/RegistroUsuarioPage/RegistroUsuarioPage.jsx
@@ -1,15 +1,17 @@
 
 import "./RegistroUsuarioPage.css"
 const RegistroUsuarioPage = () => {
-  return (
-    <div>
-    <div className="col-sm-6 px-0">
-
-    </div>
-    <h1>Registrate</h1>
+    return (
+        <div className="container">
+          <div className="row">
+            <div className="col-md-6">
+              <img src="src/assets/undraw_to_the_moon_re_q21i 1.svg" alt="Ilustración" />
+            </div>
+            <div className="col-md-6">
+            <h1>Registrate</h1>
     <hr></hr>
     <br></br>
-    <div className="col-sm-6">  
+    <div>  
         <div className="required field ">
         <label>Nombre de Usuario</label>
         <br></br>
@@ -35,13 +37,16 @@ const RegistroUsuarioPage = () => {
         <div data-lastpass-icon-root="true"></div></div>
         <br></br>
         <button type="button" className="btn btn-primary" data-bs-toggle="modal" data-bs-target="#modalCrearPlaylist" data-bs-whatever="@mdo">Registrarse</button>
-    </div>
-    </div>
+ 
+            </div>
+          </div>
+        </div>
+        </div>
+      );
       
-      
-      
-
-  )
 }
 
 export default RegistroUsuarioPage
+
+
+ 
