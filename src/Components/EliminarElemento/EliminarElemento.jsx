6d298a96aea1b5159a9b93d790c0a75bb1e173ad
Emily Callejas <emilycallejastorrico@gmail.com>
2023-10-20 22:47:47 -0400

    Tamaño Imagen-video

--- src/Components/EliminarElemento/EliminarElemento.jsx
+++ src/Components/EliminarElemento/EliminarElemento.jsx
@@ -56,11 +56,11 @@ export const EliminarElemento = ({ Imagen, Titulo, IdPlaylist, IdElemento }) => 
                       >
                         <path
                           d="M12.884 2.532C12.538 1.878 11.462 1.878 11.116 2.532L2.11596 19.532C2.03494 19.6844 1.99478 19.8551 1.99938 20.0276C2.00398 20.2001 2.05319 20.3685 2.14221 20.5164C2.23124 20.6642 2.35704 20.7864 2.50736 20.8712C2.65768 20.956 2.82739 21.0003 2.99996 21H21C21.1724 21.0004 21.342 20.956 21.4922 20.8713C21.6424 20.7866 21.7681 20.6644 21.8571 20.5167C21.946 20.3689 21.9951 20.2006 21.9997 20.0282C22.0042 19.8559 21.964 19.6852 21.883 19.533L12.884 2.532ZM13 18H11V16H13V18ZM11 14V9H13L13.001 14H11Z"
-                          fill="#F2F2F2"
+                          fill="#FFFF00"
                         />
                       </svg>
                     </div>
-                    <div className="p-2 flex-fill">
+                    <div className="p-2 flex-colum">
                       <label htmlFor="basic-url" className="form-label">
                         ¿Estás seguro de eliminar este elemento de tu Playlist?
                       </label>
@@ -69,29 +69,13 @@ export const EliminarElemento = ({ Imagen, Titulo, IdPlaylist, IdElemento }) => 
                         className="col-form-label"
                       ></label>
                       <div className="form-floating">
-                        <label
-                          htmlFor="message-text"
-                          className="col-form-label"
-                        ></label>
                         <div className="row mb-3">
                           <div className="p-2 d-flex align-items-center body-content-modal">
-                            <div className="form-floating">
-                              <div className="input-group input-group-lg">
-                                <span
-                                  className="input-group-text"
-                                  id="inputGroup-sizing-lg"
-                                >
-                                  <img src={Imagen} className="img-fluid" alt="..." />
-                                </span>
-
-                                <div>
-                                  <p className="d-flex align-items-center text-Titulo">
+                            <div>
+                              <img src={Imagen} className="img-fluid" alt="..." />
+                              <p className="d-flex align-items-center text-Titulo">
                                     {Titulo}
-                                  </p>
-                                </div>
-
-
-                              </div>
+                              </p>
                             </div>
                           </div>
                         </div>
