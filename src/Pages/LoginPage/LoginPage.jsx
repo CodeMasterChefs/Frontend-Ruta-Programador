6466c6ef0e2fea7cd70f8c54e5371db92f2b0050
Giulianno Morales <202107698@est.umss.edu>
2023-10-25 03:10:34 +0200

    Maquetado de inicioSesion2.0

--- src/Pages/LoginPage/LoginPage.jsx
+++ src/Pages/LoginPage/LoginPage.jsx
@@ -2,7 +2,54 @@
 
 const LoginPage = () => {
   return (
-    <div>LoginPage</div>
+    <div className="container">
+   
+    <div className="row">
+      <div className="col-md-7"></div>
+      <div className="col-md-5">
+        <h3>Iniciar Sesión:</h3>
+        <hr className="line-header"></hr>
+        <br></br>
+        <form>
+          <div className="required field ">
+            <label className="form-title">Correo:</label>
+            <br></br>
+            <input
+              type="text"
+              className="input-box"
+            />
+          </div>
+          <div className="required field ">
+            <label className="form-title">Contraseña:</label>
+            <br></br>
+            <input
+              type="text"
+              className="input-box"
+            />
+          </div>
+          <a href="">¿Olvidaste tu contraseña?</a>
+
+        </form>
+        <div className="d-flex justify-content-end">
+          <button
+            type="submit"
+            className="btn btn-primary"
+          >
+            Ingresar
+          </button>    
+        </div>
+        <p>¿No tienes una cuenta?</p>
+        <div className="d-flex justify-content-end">
+          <button
+            type="submit"
+            className="btn btn-primary"
+          >
+            Registrate
+          </button>    
+        </div>
+      </div>
+    </div>
+  </div>
   )
 }
 
