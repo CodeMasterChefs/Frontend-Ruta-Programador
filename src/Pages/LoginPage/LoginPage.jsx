aae55450006669e3ac0c2266b6405336afdb6cda
Emily Callejas <emilycallejastorrico@gmail.com>
2023-10-28 11:38:23 -0400

    Style: Colocar imagen de fondo

--- src/Pages/LoginPage/LoginPage.jsx
+++ src/Pages/LoginPage/LoginPage.jsx
@@ -1,60 +1,65 @@
 
 import "./LoginPage.css"
+import svgAstronautra from "../../assets/Image.svg";
 const LoginPage = () => {
   return (
     <div className="container">
-   
-    <div className="row">
-      <div className="col-md-7"></div>
-      <div className="col-md-5">
-        <h3>Iniciar Sesión:</h3>
-        <hr className="line-header"></hr>
-        <br></br>
-        <form>
-          <div className="required field ">
-            <label className="form-title">Correo:</label>
-            <br></br>
-            <input
-              type="text"
-              className="input-box"
-              placeholder="Ingresar correo"
-            />
-          </div>
-          <div className="required field ">
-            <label className="form-title">Contraseña:</label>
-            <br></br>
-            <input
-              type="text"
-              className="input-box"
-              placeholder="Ingresar contraseña"
-            />
-          </div>
-          <div className="d-flex justify-content-end">
-            <a href="">¿Olvidaste tu contraseña?</a>
-          </div>
+      <img
+        className="undraw-image"
+        src={svgAstronautra}
+        alt="Ilustración"
+      />
+      <div className="row">
+        <div className="col-md-7"></div>
+        <div className="col-md-5">
+          <h3>Iniciar Sesión:</h3>
+          <hr className="line-header"></hr>
+          <br></br>
+          <form>
+            <div className="required field ">
+              <label className="form-title">Correo:</label>
+              <br></br>
+              <input
+                type="text"
+                className="input-box"
+                placeholder="Ingresar correo"
+              />
+            </div>
+            <div className="required field ">
+              <label className="form-title">Contraseña:</label>
+              <br></br>
+              <input
+                type="text"
+                className="input-box"
+                placeholder="Ingresar contraseña"
+              />
+            </div>
+            <div className="d-flex justify-content-end">
+              <a href="">¿Olvidaste tu contraseña?</a>
+            </div>
 
-        </form>
-        <div className="d-flex justify-content-center">
-          <button
-            type="submit"
-            className="btn btn-primary"
-          >
-            Ingresar
-          </button>    
-        </div>
-        <div className="d-flex justify-content-center">
-          <p>¿No tienes una cuenta?</p>
-        </div>
-        <div className="d-flex justify-content-center">
-          <button
-            type="submit"
-            className="btn btn-primary"
-          >
-            Registrate
-          </button>    
+          </form>
+          <div className="d-flex justify-content-center">
+            <button
+              type="submit"
+              className="btn btn-primary"
+            >
+              Ingresar
+            </button>    
+          </div>
+          <div className="d-flex justify-content-center">
+            <p>¿No tienes una cuenta?</p>
+          </div>
+          <div className="d-flex justify-content-center">
+            <button
+              type="submit"
+              className="btn btn-primary"
+            >
+              Registrate
+            </button>    
+          </div>
         </div>
       </div>
-    </div>
   </div>
   )
 }
