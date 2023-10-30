48db4653431dc9b9085f26c6cac494a4691b5e1f
Emily Callejas <emilycallejastorrico@gmail.com>
2023-10-30 17:41:01 -0400

    fix: position-button and size-icon

--- src/Components/EliminarElemento/EliminarElemento.jsx
+++ src/Components/EliminarElemento/EliminarElemento.jsx
@@ -46,8 +46,8 @@ export const EliminarElemento = ({ Imagen, Titulo, IdPlaylist, IdElemento }) => 
               <div className="modal-body iconPadding ">
                 <div className="p-2  iconPeligro">
                       <svg
-                        width="24"
-                        height="24"
+                        width="30"
+                        height="30"
                         viewBox="0 0 24 24"
                         fill="none"
                         xmlns="http://www.w3.org/2000/svg"
