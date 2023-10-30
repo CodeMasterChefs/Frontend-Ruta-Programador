6cdd68fbd1a2ca29a7edc82010f304a10978c126
Nahuel <nahueltorrezg@gmail.com>
2023-10-30 00:22:15 -0400

    add: Maquetacion de HomePage

--- src/Pages/HomePage/HomePage.jsx
+++ src/Pages/HomePage/HomePage.jsx
@@ -1,9 +1,10 @@
 import SideBar from "../../Components/SideBar/SideBar";
 import NavBar from "../../Components/NavBar/NavBar";
 import { useAuth } from "../../context/AuthContext";
+import { Link } from "react-router-dom";
 
-const HomePage = ({children}) => {
-  const {isAuthenticated} = useAuth();
+const HomePage = ({ children }) => {
+  const { isAuthenticated } = useAuth();
   return (
     <>
       {isAuthenticated ? (
@@ -13,13 +14,36 @@ const HomePage = ({children}) => {
             <div className="col-sm-3 px-0">
               <SideBar />
             </div>
-            <div className="col-sm-9">
-              {children}
-            </div>
+            <div className="col-sm-9">{children}</div>
           </div>
         </div>
       ) : (
-        <h1>La ruta del programador</h1>
+        <div>
+          <div className="d-flex justify-content-end">
+            <Link to="/iniciar_sesion">
+              <button>Iniciar sesión</button>
+            </Link>
+            <Link to="/registro">
+              <button>Registrarse</button>
+            </Link>
+          </div>
+          <p>
+            <b>La ruta del programador</b>
+          </p>
+          <div className="container">
+            <div className="content">
+              <p>
+                En el vasto universo digital, el camino para aquellos que desean
+                adentrarse en el fascinante mundo de la programación, cada clic
+                es un paso más hacia la maestría en el arte de la programación.
+                Con guías detalladas, tutoriales interactivos y una comunidad de
+                entusiastas dispuestos a compartir su conocimiento, el
+                aprendizaje se convierte en un viaje colaborativo y
+                enriquecedor.
+              </p>
+            </div>
+          </div>
+        </div>
       )}
     </>
   );
