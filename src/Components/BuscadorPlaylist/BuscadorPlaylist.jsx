e060f63228adc47282eddcb38a3c90ef85922bb7
Nahuel <nahueltorrezg@gmail.com>
2023-11-12 17:32:09 -0400

    fix: valor de texto en el input

--- src/Components/BuscadorPlaylist/BuscadorPlaylist.jsx
+++ src/Components/BuscadorPlaylist/BuscadorPlaylist.jsx
@@ -1,31 +1,17 @@
 import "./BuscadorPlaylist.css";
 import "./BDtest.js";
-import { useState, useRef, useEffect } from "react";
+import { useState} from "react";
 import { sortedNames } from "./BDtest";
 
 const BuscadorPlaylist = () => {
   const [searchText, setSearchText] = useState("");
   const [suggestions, setSuggestions] = useState([]);
   const [showSuggestions, setShowSuggestions] = useState(false);
-  const inputRef = useRef(null);
-  const [isMouseOverInput, setIsMouseOverInput] = useState(false);
-
-  useEffect(() => {
-    document.addEventListener("mousedown", handleDocumentClick);
-    inputRef.current.addEventListener("mouseenter", handleInputMouseEnter);
-    inputRef.current.addEventListener("mouseleave", handleInputMouseLeave);
-
-    return () => {
-      document.removeEventListener("mousedown", handleDocumentClick);
-      inputRef.current.removeEventListener("mouseenter", handleInputMouseEnter);
-      inputRef.current.removeEventListener("mouseleave", handleInputMouseLeave);
-    };
-  }, []);
+  const isMouseOverInput = (false);
 
   const handleInputChange = (event) => {
     const inputText = event.target.value;
     setSearchText(inputText);
-
     const matchingNames = sortedNames.filter((name) =>
       name.toLowerCase().includes(inputText.toLowerCase())
     );
@@ -58,41 +44,17 @@ const BuscadorPlaylist = () => {
   const handleInputBlur = () => {
     setTimeout(() => {
       if (!isMouseOverInput) {
-        if (suggestions.length != 1) {
-          handleSuggestionClick(suggestions[0]);
-        } else {
-          setSearchText("");
-          setSuggestions([]);
-          setShowSuggestions(false);
-        }
+        setSearchText("");
+        setSuggestions([]);
+        setShowSuggestions(false);
       }
-    }, 100);
+    }, 500);
   };
 
   const handleSearchButtonClick = () => {
     if (searchText.length > 0) {
       console.log("Valor del input:", searchText);
     }
-  };
-
-
-  
-  const handleDocumentClick = (event) => {
-    if (inputRef.current && !inputRef.current.contains(event.target)) {
-      setTimeout(() => {
-        setSearchText("");
-        setSuggestions([]);
-        setShowSuggestions(false);
-      }, 1);
-    }
-  };
-
-  const handleInputMouseEnter = () => {
-    setIsMouseOverInput(true);
-  };
-
-  const handleInputMouseLeave = () => {
-    setIsMouseOverInput(false);
   };
 
   return (
@@ -117,11 +79,12 @@ const BuscadorPlaylist = () => {
           onChange={handleInputChange}
           onFocus={handleInputFocus}
           onBlur={handleInputBlur}
-          ref={inputRef}
         />
         <button
           type="button"
-          onClick={handleSearchButtonClick}
+          onClick={() => {
+            handleSearchButtonClick();
+          }}
           className="button-search-playlist"
         >
           <i className="fa fa-search"></i>
