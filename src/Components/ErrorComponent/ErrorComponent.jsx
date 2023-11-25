dd2a2b432259d54508c92e50be0b1483856434e7
Emily Callejas <emilycallejastorrico@gmail.com>
2023-11-25 12:49:56 -0400

    Fix: posición mensaje de error

--- src/Components/ErrorComponent/ErrorComponent.jsx
+++ src/Components/ErrorComponent/ErrorComponent.jsx
@@ -3,26 +3,27 @@ import AstronautaPerdido from '../../assets/Astronauta_perdido.png';
 import './ErrorComponent.css';
 
 const ErrorComponent = ({ children }) => {
-    return (
-  <div className="container-fluid height-error_component d-flex">
-    <div className="row d-flex align-items-center justify-content-center">
-      <div className="col-md-6 text-center animate-fadeIn">
-        <img
-          src={AstronautaPerdido}
-          alt="Astronauta perdido"
-          className="animate-shakeX w-75 mx-auto" // Agrega la clase mx-auto
-        />
-      </div>
-      <div className="col-md-6 text-center">
-        <h4 className="mt-3">{children}</h4>
+  return (
+    <div className="container-fluid height-error_component d-flex align-items-start">
+      <div className="row d-flex align-items-center justify-content-center container-error-text-image">
+        <div className="col-md-6 text-center">
+          <h4 className="mt-3">{children}</h4>
+        </div>
+        <div className="col-md-6 text-center animate-fadeIn">
+          <img
+            src={AstronautaPerdido}
+            alt="Astronauta perdido"
+            className="animate-shakeX w-75 mx-auto" // Agrega la clase mx-auto
+          />
+        </div>
+
       </div>
     </div>
-  </div>
-);
+  );
 };
 
 ErrorComponent.propTypes = {
-    children: PropTypes.node
+  children: PropTypes.node
 };
 
 export default ErrorComponent;
